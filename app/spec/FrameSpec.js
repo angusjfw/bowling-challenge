605e366390abf979cdcfaea7a8427e9ca45cfcfb
hibreez <4ngus.whitehead@gmail.com>
2016-01-10 04:04:30 +0000

    adds frame.bowl() function which reduces rremainingPins

--- app/spec/FrameSpec.js
+++ app/spec/FrameSpec.js
@@ -5,9 +5,34 @@ describe("Frame", function() {
     frame = new Frame();
   });
 
-  describe("#pinsRemaining", function(){
+  describe("#remainingPins", function() {
     it("is set to 10 initally", function() {
-      expect(frame.pinsRemaining()).toEqual(10);
+      expect(frame.remainingPins()).toEqual(10);
     });
+  });
+
+  describe("#bowl", function() {
+    it("decreases remainingPins by a random number <= 10", function() {
+      spyOn(Math, "random").and.returnValue(4/10);
+      frame.bowl();
+      expect(frame.remainingPins()).toEqual(6);
+    });
+    
+    it("decreases remainingPins by a number <= remainingPins", function() {
+      spyOn(Math, "random").and.returnValues(5/10, 5/5);
+      frame.bowl();
+      console.log(frame.remainingPins());
+      frame.bowl();
+      console.log(frame.remainingPins());
+      expect(frame.remainingPins()).toEqual(0);
+    });
+
+    it("cannot decrease remaningPins below 0", function() {
+      spyOn(Math, "random").and.returnValue(1);
+      frame.bowl();
+      frame.bowl();
+      expect(frame.remainingPins()).toEqual(0);
+    });
+
   });
 });
--- app/src/frame.js
+++ app/src/frame.js
@@ -1,7 +1,12 @@
 function Frame() {
-  this._pinsRemaining = 10;
+  this._remainingPins = 10;
 }
 
-Frame.prototype.pinsRemaining = function() {
-  return 10;
+Frame.prototype.remainingPins = function() {
+  return this._remainingPins;
+}
+
+Frame.prototype.bowl = function() {
+  var pinsHit = Math.round(Math.random() * this.remainingPins());
+  this._remainingPins = this.remainingPins() - pinsHit;
 }
